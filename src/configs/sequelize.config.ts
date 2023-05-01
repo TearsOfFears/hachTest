89b65f55249583ce07f64c4b223bbf5bd045a310
Nazar Demchan <name.surname@plus4u.net>
2023-05-01 16:56:58 +0300

    fix prod env

--- src/configs/sequelize.config.ts
+++ src/configs/sequelize.config.ts
@@ -2,24 +2,30 @@ import { SequelizeModuleAsyncOptions } from '@nestjs/sequelize';
 import { ConfigService } from '@nestjs/config';
 export const getSequelizeConfig = (): SequelizeModuleAsyncOptions => {
   return {
-    useFactory: (configService: ConfigService) => ({
-      dialect: configService.get('DB_DIALECT'),
-      host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
-      username: configService.get('DB_USER'),
-      password: configService.get('DB_PASS'),
-      database: configService.get('DB_NAME_DEVELOPMENT'),
-      autoLoadModels: true,
-      synchronize: true,
-      logging: true,
-      // dialectOptions: {
-      //   ssl: {
-      //     require: true,
-      //     rejectUnauthorized: false,
-      //   },
-      //   // native: true,
-      // },
-    }),
+    useFactory: (configService: ConfigService) => {
+      const configs = {
+        dialect: configService.get('DB_DIALECT'),
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_NAME_DEVELOPMENT'),
+        autoLoadModels: true,
+        synchronize: true,
+        logging: true,
+        dialectOptions: {},
+      };
+      if (configService.get('ENV') === 'PROD') {
+        configs.dialectOptions = {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+          native: true,
+        };
+      }
+      return configs;
+    },
     inject: [ConfigService],
   };
 };
